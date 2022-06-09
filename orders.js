9d34eae9e9a5432d9370624a9b4f08e422e8fcd0
TanTM <tantranminh2803@gmail.com>
2022-06-10 01:36:30 +0700

    first commit

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "13133",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "13133",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "13133",
+    paymentStatus: "Due",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "13133",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "13133",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "13133",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+];
